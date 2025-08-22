6a8fb8df823246da11f3c5b3bdf535d3b22a067c
Todaywyou <srirezekirahayu2424@gmail.com>
2025-08-22 10:30:24 +0700

    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { Routes } from "react-router";
+import { Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
 
 export default function APP() {
   return(
     <Routes>
-
+      <Route path="/" element ={<HomePage />} />
     </Routes>
   );
 }
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+import Footer from "../components/Footer";
+
+export default function HomePage() {
+  return (
+    <div>
+       <Footer/>
+    </div>
+  );
+}
